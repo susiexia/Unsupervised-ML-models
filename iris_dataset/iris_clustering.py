5887ade10c40f3d60518b24bcba62c523c562d86
susiexia <57109686+susiexia@users.noreply.github.com>
2020-03-25 22:40:20 -0700

    update claster on iris_dataset

---
+++ iris_dataset/iris_clustering.py
@@ -0,0 +1,51 @@
+# %% [markdown]
+# K-means: an unsupervised learning algorithm used to identify and solve clustering issues.
+# A centroid is a data point that is the arithmetic mean position of all the points on a cluster
+
+# %%
+import pandas as pd 
+from sklearn.cluster import KMeans
+import plotly.express as px
+import hvplot.pandas
+
+# %%
+# cleaned dataset loading
+iris_df = pd.read_csv('new_iris.csv')
+iris_df.head(3)
+# %%
+# Init the K starting centroids, with K =3
+model = KMeans(n_clusters=3, random_state= 5)
+
+# data points assigned to nearest centroid
+# (When the model is being trained (fit the data), the K-means algorithm will iteratively look for the best centroid for each of the K clusters)
+model.fit(iris_df)
+
+# group data points
+predictions = model.predict(iris_df)
+predictions
+
+# %%
+# get info about three centroid coordinates
+model.cluster_centers_
+# %%
+# add predictions outcome back to original dataframe
+iris_df['class'] = model.labels_
+
+#iris_df['class'] = predictions
+iris_df.head()
+
+# %% [markdown]
+# data Visualization of results
+
+# %%
+# hvplot to 2D scatter 
+import hvplot.pandas
+iris_df.hvplot.scatter(x='petal_width', y='sepal_length', by ='class')
+
+# %%
+fig = px.scatter_3d(iris_df, x='petal_width', y='sepal_length', z='petal_length',
+                    color='class', symbol='class', size='sepal_width', width=800)
+fig.update_layout(legend = dict(x=0, y=1)) # update the legend position to legend = {'x': 0, 'y':1}
+fig.show()
+
+# %%
