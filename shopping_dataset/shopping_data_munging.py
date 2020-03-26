e8900c8730fb104bfb6a3d877a19e69179130f8c
susiexia <57109686+susiexia@users.noreply.github.com>
2020-03-25 18:45:48 -0700

    finish data munging steps by using pandas

--- shopping_dataset/shopping_data_munging.py
+++ shopping_dataset/shopping_data_munging.py
@@ -29,4 +29,28 @@ shopping_df = shopping_df.dropna()
 shopping_df.drop(columns=['CustomerID'], inplace=True)
 
 shopping_df.head()
+# %% [markdown]
+# data Processing
+# QUESTIONS: Is the data in a format 
+# that can be passed into an unsupervised learning model?
+
 # %%
+# convert categorical into numerical data
+# define a function
+shopping_df['Card Member'] = shopping_df['Card Member'].apply(lambda a: 1 if a=='Yes' else 0)
+
+# rescale annual income
+shopping_df['Annual Income'] = shopping_df['Annual Income'] / 1000
+
+# %%
+# rename columns
+shopping_df.rename(columns={'Card Member': 'Card_Member', 'Annual Income': 'Annual_Income',
+                    'Spending Score (1-100)': 'Spending_Score_(1-100)'}, inplace= True)
+
+shopping_df.head()
+
+# %% [markdown]
+# data Transformation 
+# QUESTIONS: Can I quickly hand off this data for others to use?
+# to readable and user-friendly format
+shopping_df.to_csv('shopping_data_cleaned.csv', index = False)
